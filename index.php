51f8642abbb67ba913f145eee10719e8c08d0b3a
John DiBaggio <jsantoku@gmail.com>
2014-03-31 17:06:44 -0500

    deleted stuff
    deleted stuff

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php include "protected/extensions/twitteroauth.php"; ?>
-<?php  
-$consumer = "IRwjT5WEXoUALEei4czUg";
-$consumersecret = "UEt6lrKBj5J9tferFwwrniEtmiC983qCNpzA84FiOM";
-$accesstoken = "1836126486-hud3pfXw9TLRoFE6ajPeSGwNp3ohMv2KHGB8qQI";
-$accesstokensecret = "zmPf1cQOFfjebsN2JqSJ0itMXeeqsNyWEVZmj76ekXulV";
 
-$twitter = new TwitterOAuth($consumer, $consumersecret, $accesstoken, $accesstokensecret);
-
-?>
 <?php
 
 // change the following paths if necessary
